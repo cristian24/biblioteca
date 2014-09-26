fcbe2c7c0b4f97505776eb4f9ac7dbbfdaef394c
cristian24 <cristian.cuspoca@correounivalle.edu.co>
2014-09-25 23:10:00 -0500

    autores completo
    modúlo de autores completo

--- application/controllers/autores.php
+++ application/controllers/autores.php
@@ -59,6 +59,78 @@ class Autores extends CI_Controller {
 		$this->load->view('autores/create');
 	}
 
+    public function update_rqst($id)
+    {
+        if($this->input->is_ajax_request())
+        {            
+            $this->form_validation->set_rules('nombre', 'Nombre Autor', 'trim|required|min_length[3]|max_length[30]|xss_clean');
+            if($this->form_validation->run() === FALSE)
+            {
+                $data = array('res' => 'error', 'name' => form_error('nombre'));
+                echo json_encode($data);  
+                return false;
+            }else
+            {
+                $nombre = $this->input->post('nombre');
+                $peticion = $this->autores_model->update($id,$nombre);
+                $data = array('res' => 'success', 'rqst' => $peticion);
+                echo json_encode($data);
+            }
+        }else
+        {
+            show_404();
+        }
+    }
+
+    public function query_rqst($query=FALSE)
+    {
+        if($this->input->is_ajax_request())
+        {
+            if($query === 'TODO')
+            {
+                $response = $this->autores_model->get_autores();
+                $data = array('res' => 'success', 'datos' => $response);
+                echo json_encode($data);
+            }else
+            {
+                $response = $this->autores_model->get_name_id($query);
+                if( ! empty($response))
+                {
+                    $data = array('res' => 'success', 'datos' => $response);
+                    echo json_encode($data);
+                }else
+                {
+                    $data = array('res' => 'no found');
+                    echo json_encode($data);
+                }
+            }            
+        }else
+        {
+            show_404();
+        }
+    }
+
+    public function query()
+    {
+        $this->session->acceso('Catalogador');
+        $data['libros_class'] = 'active';
+        $data['usuario_class'] = '';
+        $data['libro_create_class'] = '';
+        $data['autor_create_class'] = '';
+        $data['autor_edit_class'] = 'active';
+        $data['editorial_create_class'] = '';
+        $data['editorial_edit_class'] = '';
+        $data['title'] = 'Modificar Autor';
+        $data['title_section'] = 'Modificar Autor';
+        $data['subtitle_section'] = 'Consulta, Elije y Actualiza';
+        $data['section_actual'] = 'Modificar Autor';
+        $data['libro_query_class'] = '';
+        $data['autores'] = $this->autores_model->get_autores();
+        $this->load->view('template/header', $data);
+        $this->load->view('autores/query', $data);
+        $this->load->view('template/footer');
+    }
+
 }
 
 /* End of file autores.php */
