0997b5be16c4ac83b9ddd2f598f45b8bd23b378e
cristian24 <cristian.cuspoca@correounivalle.edu.co>
2014-11-16 10:24:21 -0500

    Cambio Correo
    Cambio de correo

--- application/config/email.php
+++ application/config/email.php
@@ -2,8 +2,8 @@
     $config['protocol'] = 'smtp';
     $config['smtp_host'] = 'ssl://smtp.gmail.com';
     $config['smtp_port'] = 465;
-    $config['smtp_user'] = 'pruebasappcristian@gmail.com';
-    $config['smtp_pass'] = 'cuentaCorreoApp789';
+    $config['smtp_user'] = 'correo@gmail.com';
+    $config['smtp_pass'] = 'contrasena';
     $config['mailtype'] = 'html';
     $config['charset'] = 'utf-8';
     $config['newline'] = "\r\n";
